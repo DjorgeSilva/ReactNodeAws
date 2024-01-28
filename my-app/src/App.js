bb219b288f2f7a9728f6d56f6c05e6f9133bc525
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-28 15:41:48 -0300

    Add footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,35 @@
-import { Authenticator, Image, View, useTheme } from "@aws-amplify/ui-react";
+import {
+  Authenticator,
+  Image,
+  Text,
+  View,
+  useTheme,
+} from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 export default function App() {
-  const {
-    tokens: {
-      space: { small },
-    },
-  } = useTheme();
+  const { tokens } = useTheme();
 
   const components = {
     Header() {
       return (
-        <View textAlign="center" padding={small}>
+        <View textAlign="center" padding={tokens.space.small}>
           <Image
             alt="stefanini-logo"
             src={require("./assets/logo-stefanini.png")}
           />
+        </View>
+      );
+    },
+    Footer() {
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.neutral[80]}>
+            &copy; Stefanini Group - Todos os direitos reservados
+          </Text>
         </View>
       );
     },
