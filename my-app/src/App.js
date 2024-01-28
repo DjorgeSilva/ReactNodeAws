216a2b7440322d12b708c6b28fb88fb55c615c6a
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-28 15:38:03 -0300

    Add header: use stefanini logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,31 @@
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, Image, View, useTheme } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 export default function App() {
+  const {
+    tokens: {
+      space: { small },
+    },
+  } = useTheme();
+
+  const components = {
+    Header() {
+      return (
+        <View textAlign="center" padding={small}>
+          <Image
+            alt="stefanini-logo"
+            src={require("./assets/logo-stefanini.png")}
+          />
+        </View>
+      );
+    },
+  };
+
   return (
-    <Authenticator loginMechanism={["email"]}>
+    <Authenticator loginMechanism={["email"]} components={components}>
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user.username}</h1>
