b5930166ff23a435b2a07b6d645098cb1ca05caa
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-28 15:15:48 -0300

    use Authentication from aws

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">My App</div>;
-}
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
-export default App;
+export default function App() {
+  return (
+    <Authenticator loginMechanism={["email"]}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
