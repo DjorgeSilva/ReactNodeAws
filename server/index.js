7af8bb1e116ac9820543a6dcad82473ae5ffdf54
Flânelon DJorge Silva Salazar <flanelon18@gmail.com>
2024-01-29 13:00:11 -0300

    update server use cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const user = require("./routes");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).json({ success: true });
